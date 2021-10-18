146870e6710b5d6e70e39906e5631632247a8a65
Angelo <62099585+acmachado14@users.noreply.github.com>
2021-10-18 16:28:15 -0300

    removed duplicade codes

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -6,55 +6,8 @@ use App\Models\Carro;
 
 class CarroController extends Controller
 {
-    public function index() {
-
-        return Carro::all();
-    }
-
-
-    public function store(Request $request)
+    public function __construct()
     {
-        return response()
-            ->json(
-                Carro::create($request->all()),
-                201
-            );
+        $this->classe = Carro::class;
     }
-  
-    public function show(int $id)
-    {
-        $Carro = Carro::find($id);
-        if (is_null($Carro)) {
-            return response()->json('', 204);
-        }
-    
-        return response()->json($Carro);
-    }
-    
-    public function update(int $id, Request $request)
-    {
-        $Carro = Carro::find($id);
-        if (is_null($Carro)) {
-            return response()->json([
-                'erro' => 'Recurso não encontrado'
-            ], 404);
-        }
-        $Carro->fill($request->all());
-        $Carro->save();
-    
-        return $Carro;
-    }
-    
-    public function destroy(int $id)
-    {
-        $qtdRecursosRemovidos = Carro::destroy($id);
-        if ($qtdRecursosRemovidos === 0) {
-            return response()->json([
-                'erro' => 'Recurso não encontrado'
-            ], 404);
-        }
-    
-        return response()->json('', 204);
-    }
-
 }
--- app/Http/Controllers/DebitoController.php
+++ app/Http/Controllers/DebitoController.php
@@ -6,55 +6,8 @@ use App\Models\Debito;
 
 class DebitoController extends Controller
 {
-    public function index() {
-
-        return Debito::all();
-    }
-
-
-    public function store(Request $request)
+    public function __construct()
     {
-        return response()
-            ->json(
-                Debito::create($request->all()),
-                201
-            );
+        $this->classe = Debito::class;
     }
-  
-    public function show(int $id)
-    {
-        $Debito = Debito::find($id);
-        if (is_null($Debito)) {
-            return response()->json('', 204);
-        }
-    
-        return response()->json($Debito);
-    }
-    
-    public function update(int $id, Request $request)
-    {
-        $Debito = Debito::find($id);
-        if (is_null($Debito)) {
-            return response()->json([
-                'erro' => 'Recurso não encontrado'
-            ], 404);
-        }
-        $Debito->fill($request->all());
-        $Debito->save();
-    
-        return $Debito;
-    }
-    
-    public function destroy(int $id)
-    {
-        $qtdRecursosRemovidos = Debito::destroy($id);
-        if ($qtdRecursosRemovidos === 0) {
-            return response()->json([
-                'erro' => 'Recurso não encontrado'
-            ], 404);
-        }
-    
-        return response()->json('', 204);
-    }
-
 }
